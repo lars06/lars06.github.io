174506abe037652ad75cef7192c83e952869bb4f
lars <larsmellick@me.com>
2024-01-14 15:21:33 +1100

    Add nav bar without working links

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,12 @@
 import React from 'react';
 import './App.css';
-import cliffs from './resources/background-images/cliffs.jpeg';
-import stairs from './resources/background-images/stairs.jpeg';
-import piano from './resources/background-images/piano.jpeg';
-import watering from './resources/background-images/watering.jpeg';
-import lonely from './resources/background-images/lonely.jpeg';
-import bridge from './resources/background-images/bridge.jpeg';
-import blanc from './resources/background-images/blanc.jpeg';
-import jcm from './resources/background-images/jcm.jpeg';
-import sailor from './resources/background-images/sailor.gif';
-import acappella from './resources/background-images/acappella.gif';
-import bike from './resources/background-images/bike.jpeg';
-import building from './resources/background-images/building.jpeg';
-import crane from './resources/background-images/crane.jpeg';
-import embrace from './resources/background-images/embrace.jpeg';
-import lostweekend from './resources/background-images/lostweekend.gif';
-import reflect from './resources/background-images/reflect.jpeg';
-import smoko from './resources/background-images/smoko.jpeg';
-import zoom from './resources/background-images/zoom.jpeg';
-import clothes from './resources/background-images/clothes.jpeg';
-import cloud from './resources/background-images/cloud.jpeg';
-import drip from './resources/background-images/drip.jpeg';
-import hike from './resources/background-images/hike.gif';
-import graf from './resources/background-images/graf.jpeg';
-import rolling from './resources/background-images/rolling.jpeg';
+import NavBar from './NavBar';
 import PhotoGrid from './PhotoGrid';
 
 function App() {
   return (
     <div className="App">
+      <NavBar/>
       <div className="title">
           <h1 id="lars">
             lars
---
+++ src/NavBar.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+const NavBar = () => {
+  return (
+    <nav>
+      <ul>
+        <li><a href="/">home</a></li>
+        <li><a href="/">about me</a></li>
+        <li><a href="/">music</a></li>
+        <li><a href="/">contact</a></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
