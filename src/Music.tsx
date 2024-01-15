a8adc65194231dce017689d06b0c6cf6689bf142
lars <larsmellick@me.com>
2024-01-15 16:00:48 +1100

    Add initial Soundcloud items

--- src/Music.tsx
+++ src/Music.tsx
@@ -6,7 +6,19 @@ function Music() {
     <div className="content">
         <h1>music</h1>
         <h2>piano</h2>
-        <p>here is a piano improv</p>
+        <p>live recordings of piano improvisations</p>
+        <div className="soundcloudList">
+            <iframe
+              className="soundcloudFrame"
+              src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1675855665&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
+              scrolling="no">
+            </iframe>
+            <iframe
+              className="soundcloudFrame"
+              src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1634721846&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
+              scrolling="no">
+            </iframe>
+        </div>
     </div>
   );
 }
