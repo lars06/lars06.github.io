803dad03a9b49d384b830230c6afbb15f942566d
lars <larsmellick@me.com>
2024-01-15 17:57:25 +1100

    Adjust spacing

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -3,7 +3,6 @@ import '../index.css';
 
 import model1 from '../background-images/model-1.jpeg';
 import model2 from '../background-images/model-2.jpeg';
-import model3 from '../background-images/model-3.jpeg';
 import model4 from '../background-images/model-4.jpeg';
 import model5 from '../background-images/model-5.jpeg';
 import model6 from '../background-images/model-6.jpeg';
@@ -22,7 +21,6 @@ function About() {
           <ul>
             <li><img src={model1} className="small-portrait-img" alt="Pose-1"/></li>
             <li><img src={model2} className="small-portrait-img" alt="Pose-2"/></li>
-            <li><img src={model3} className="small-portrait-img" alt="Pose-3"/></li>
             <li><img src={model4} className="small-portrait-img" alt="Pose-4"/></li>
             <li><img src={model5} className="small-portrait-img" alt="Pose-5"/></li>
             <li><img src={model6} className="small-portrait-img" alt="Pose-6"/></li>
