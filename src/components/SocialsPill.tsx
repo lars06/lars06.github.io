49b709ffdd47665571188468777c9bfd76ca9eb8
lars <larsmellick@me.com>
2024-01-16 13:02:08 +1100

    Extract urls into properties

--- src/components/SocialsPill.tsx
+++ src/components/SocialsPill.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { SocialIcon } from 'react-social-icons'
 import '../index.css';
+import { properties } from '../properties';
 
 const SocialsPill = () => {
   return (
     <div className="socialsContainer">
         <div className="socialsPill">
-            <SocialIcon url="https://www.instagram.com/lars.mellick" target="_blank"/>
-            <SocialIcon url="https://soundcloud.com/larsmellick" target="_blank"/>
-            <SocialIcon url="https://linkedin.com/in/larsmellick" target="_blank"/>
-            <SocialIcon url="mailto:hi@lars.au?subject=hi from website"/>
+            <SocialIcon url={properties.instagramUrl} target="_blank"/>
+            <SocialIcon url={properties.soundcloudUrl} target="_blank"/>
+            <SocialIcon url={properties.linkedinUrl} target="_blank"/>
+            <SocialIcon url={properties.emailUrl}/>
         </div>
     </div>
   );
---
+++ src/properties.tsx
@@ -0,0 +1,6 @@
+export const properties = {
+    instagramUrl: "https://www.instagram.com/lars.mellick",
+    soundcloudUrl: "https://soundcloud.com/larsmellick",
+    linkedinUrl: "https://linkedin.com/in/larsmellick",
+    emailUrl: "mailto:hi@lars.au?subject=hi from website"
+};
