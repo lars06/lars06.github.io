52c9ce2711a861c5c0613e8cd6158b2a5b40af60
lars <larsmellick@me.com>
2024-08-01 12:45:15 +1000

    Add film score competition youtube embed

--- src/pages/Music.tsx
+++ src/pages/Music.tsx
@@ -5,17 +5,30 @@ function Music() {
   return (
     <div className="content">
         <h1>music</h1>
-        <h2>piano</h2>
+        <h2>for media</h2>
+        <p>film scoring examples</p>
+        <iframe
+            className="youtubeFrame"
+            src="https://www.youtube.com/embed/3hQE1J94uos?si=5vU_xpShasye0MeR"
+            title="Indie Film Music Contest entry 2024"
+            frameBorder="0"
+            allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerPolicy="strict-origin-when-cross-origin"
+            allowFullScreen>
+        </iframe>
+        <h2>for piano</h2>
         <p>live recordings of piano improvisations</p>
         <div className="soundcloudList">
             <iframe
               className="soundcloudFrame"
               src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1675855665&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
+              frameBorder="0"
               scrolling="no">
             </iframe>
             <iframe
               className="soundcloudFrame"
               src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1634721846&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
+              frameBorder="0"
               scrolling="no">
             </iframe>
         </div>
