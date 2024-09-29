eca4c38d501d12dc084dfebf5ca2e130ad688e3e
lars <larsmellick@me.com>
2024-09-29 13:55:59 +1000

    Add musical orchestral sample

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -16,18 +16,18 @@ function About() {
           <p>hi i'm lars and welcome to lars.au, the goto website for lars.</p>
           <p>after working as a software engineer for several years, i am currently pursuing projects in various areas of music, software, and writing.</p>
           <p>drop me a message on one of the social pipes!</p>
+          <div className="photos">
+            <ul>
+              <li><img src={action1} className="small-portrait-img" alt="Photo shoot pose 1"/></li>
+              <li><img src={action2} className="small-portrait-img" alt="Photo shoot pose 2"/></li>
+              <li><img src={action3} className="small-portrait-img" alt="Photo shoot pose 3"/></li>
+              <li><img src={action4} className="small-portrait-img" alt="Photo shoot pose 4"/></li>
+              <li><img src={action5} className="small-portrait-img" alt="Photo shoot pose 5"/></li>
+              <li><img src={action6} className="small-portrait-img" alt="Photo shoot pose 6"/></li>
+            </ul>
+          </div>
       </div>
-      <div className="photos">
-          <ul>
-            <li><img src={action1} className="small-portrait-img" alt="Photo shoot pose 1"/></li>
-            <li><img src={action2} className="small-portrait-img" alt="Photo shoot pose 2"/></li>
-            <li><img src={action3} className="small-portrait-img" alt="Photo shoot pose 3"/></li>
-            <li><img src={action4} className="small-portrait-img" alt="Photo shoot pose 4"/></li>
-            <li><img src={action5} className="small-portrait-img" alt="Photo shoot pose 5"/></li>
-            <li><img src={action6} className="small-portrait-img" alt="Photo shoot pose 6"/></li>
-          </ul>
-        </div>
-      </div>
+    </div>
   );
 }
 
--- src/pages/Music.tsx
+++ src/pages/Music.tsx
@@ -6,7 +6,7 @@ function Music() {
     <div className="content">
         <h1>music</h1>
         <h2>for media</h2>
-        <p>film scoring examples</p>
+        <p>film composition</p>
         <iframe
             className="youtubeFrame"
             src="https://www.youtube.com/embed/3hQE1J94uos?si=5vU_xpShasye0MeR"
@@ -16,17 +16,29 @@ function Music() {
             referrerPolicy="strict-origin-when-cross-origin"
             allowFullScreen>
         </iframe>
+        <p>musical theatre</p>
+        <div className="soundcloudList">
+            <iframe
+              className="soundcloudFrame"
+              title="Original Musical Song - Orchestral Arrangement (no vocals)"
+              src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1925633723&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
+              frameBorder="0"
+              scrolling="no">
+            </iframe>
+        </div>
         <h2>for piano</h2>
         <p>live recordings of piano improvisations</p>
         <div className="soundcloudList">
             <iframe
               className="soundcloudFrame"
+              title="Yin Yoga / Meditation live piano improvisation"
               src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1675855665&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
               frameBorder="0"
               scrolling="no">
             </iframe>
             <iframe
               className="soundcloudFrame"
+              title="DRIP improv"
               src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1634721846&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
               frameBorder="0"
               scrolling="no">
