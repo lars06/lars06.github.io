445c97a40d33de5e5e0b4299dbd5fe57b0b688e4
lars <larsmellick@me.com>
2025-08-10 18:19:08 +1000

    Change instagram username

--- src/properties.tsx
+++ src/properties.tsx
@@ -1,5 +1,5 @@
 export const properties = {
-    instagramUrl: "https://www.instagram.com/lars.mellick",
+    instagramUrl: "https://www.instagram.com/lars.sounds",
     soundcloudUrl: "https://soundcloud.com/larsmellick",
     linkedinUrl: "https://linkedin.com/in/larsmellick",
     emailUrl: "mailto:hi@lars.au?subject=hi from website",
